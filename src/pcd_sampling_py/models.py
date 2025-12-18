13d5970f30a3229027d78c10133132a67851935d
Petr Novikov <novikov@i59-r143-pool02.isas-stud.iar.kit.edu>
2025-12-18 19:17:48 +0100

    updated docs

--- src/pcd_sampling_py/models.py
+++ src/pcd_sampling_py/models.py
@@ -2,6 +2,9 @@ from dataclasses import dataclass
 
 @dataclass
 class PCDSamplerConfig:
+    """
+    Config class for the sampler.
+    """
     dim: int
     number_unit_vectors: int
     number_samples: int
--- src/pcd_sampling_py/sampler.py
+++ src/pcd_sampling_py/sampler.py
@@ -2,17 +2,26 @@ from typing import Callable
 import torch
 from torch import Tensor
 from pcd_sampling_py.models import PCDSamplerConfig
-from pcd_sampling_py.sampling_utils import gm_cdf_1d, gm_pdf_1d, heaviside_mean, sample_gm, sample_gm_cached_cholesky
+from pcd_sampling_py.sampling_utils import (
+    gm_cdf_1d,
+    gm_pdf_1d,
+    heaviside_mean,
+    sample_gm,
+    sample_gm_cached_cholesky,
+)
+
 
 class PCDSampler:
-    """Projected Cumulative Distribution sampling."""
+    """
+    Deterministic sampling using Projected Cumulative Distribution.
+
+    L - number of samples
+    N - dimension
+    K - number of unit vectors
+    T - number of components in a GM
+    """
 
     def __init__(self, config: PCDSamplerConfig):
-        """
-        :param self: Description
-        :param dim: dimensions of the unit vectors (dimensions of what we sample from).
-        :param K: number of unit vectors
-        """
         self.number_samples = config.number_samples
         self.number_unit_vectors = config.number_unit_vectors
         self.threshold = config.threshold
@@ -24,10 +33,18 @@ class PCDSampler:
         self.alpha_min = 0.01
 
         # Batch functions for calculating gain.
-        self._delta_r_vmap = torch.func.vmap(self.calculate_delta_r, in_dims=(0, None, None, None, None))
-        self._delta_x_vmap = torch.func.vmap(self.calculate_delta_x, in_dims=(0, 0, 0, 0, None))
-        self._delta_r_sorted_vmap = torch.func.vmap(self.calculate_delta_r_sorted, in_dims=(0, 0, None, None, None))
-        self._delta_x_sorted_vmap = torch.func.vmap(self.calculate_delta_x_sorted, in_dims=(0, 0, 0, 0, None))
+        self._delta_r_vmap = torch.func.vmap(
+            self.calculate_delta_r, in_dims=(0, None, None, None, None)
+        )
+        self._delta_x_vmap = torch.func.vmap(
+            self.calculate_delta_x, in_dims=(0, 0, 0, 0, None)
+        )
+        self._delta_r_sorted_vmap = torch.func.vmap(
+            self.calculate_delta_r_sorted, in_dims=(0, 0, None, None, None)
+        )
+        self._delta_x_sorted_vmap = torch.func.vmap(
+            self.calculate_delta_x_sorted, in_dims=(0, 0, 0, 0, None)
+        )
 
         # If sorting of the projections is enabled use the correct impelementation
         if self.sorting:
@@ -46,13 +63,19 @@ class PCDSampler:
         Randomly creates unit vectors to project the distribution onto.
         """
 
-        self.unit_vectors = torch.distributions.uniform.Uniform(-1.0, 1.0).sample((self.number_unit_vectors, self.dim))
-        self.unit_vectors = self.unit_vectors / self.unit_vectors.norm(dim=1, keepdim=True)
+        self.unit_vectors = torch.distributions.uniform.Uniform(-1.0, 1.0).sample(
+            (self.number_unit_vectors, self.dim)
+        )
+        self.unit_vectors = self.unit_vectors / self.unit_vectors.norm(
+            dim=1, keepdim=True
+        )
 
     def create_unit_vectors_deterministic(self): ...
 
     @torch.compile
-    def calculate_delta_r(self, x: Tensor, r: Tensor, weights: Tensor, means: Tensor, stds: Tensor):
+    def calculate_delta_r(
+        self, x: Tensor, r: Tensor, weights: Tensor, means: Tensor, stds: Tensor
+    ):
         """
         This function is used to compute the gain between real and approximation projected distributions
 
@@ -73,7 +96,9 @@ class PCDSampler:
         return -((F_tilda - F) / torch.clamp(f_tilda, min=eps))
 
     @torch.compile
-    def calculate_delta_r_sorted(self, x: Tensor, i: Tensor, weights: Tensor, means: Tensor, stds: Tensor):
+    def calculate_delta_r_sorted(
+        self, x: Tensor, i: Tensor, weights: Tensor, means: Tensor, stds: Tensor
+    ):
         """
         This function is used to compute the gain between real and approximation projected distributions for sorted projections.
 
@@ -89,10 +114,15 @@ class PCDSampler:
         f_tilda = gm_pdf_1d(x, weights, means, stds)
 
         eps = 1e-3  # to avoid division by zero.
-        return -((F_tilda - (2 * i - 1) / 2 / self.number_samples) / torch.clamp(f_tilda, min=eps))
+        return -(
+            (F_tilda - (2 * i - 1) / 2 / self.number_samples)
+            / torch.clamp(f_tilda, min=eps)
+        )
 
     @torch.compile
-    def calculate_delta_x(self, r: Tensor, u: Tensor, means: Tensor, stds: Tensor, weights: Tensor):
+    def calculate_delta_x(
+        self, r: Tensor, u: Tensor, means: Tensor, stds: Tensor, weights: Tensor
+    ):
         """
         This function is used to compute the gain for all samples with respect to the provided unit vector.
 
@@ -108,7 +138,9 @@ class PCDSampler:
         return delta_x
 
     @torch.compile
-    def calculate_delta_x_sorted(self, r: Tensor, u: Tensor, means: Tensor, stds: Tensor, weights: Tensor):
+    def calculate_delta_x_sorted(
+        self, r: Tensor, u: Tensor, means: Tensor, stds: Tensor, weights: Tensor
+    ):
         """
         This function is used to compute the gain for all samples with respect to the provided unit vector.
         It first sorts the projections for the unit vector
@@ -122,22 +154,36 @@ class PCDSampler:
 
         sorted_r, idx = torch.sort(r)
 
-        delta_r_sorted = self._delta_r_sorted_vmap(sorted_r, self.numbers, weights, means, stds)
+        delta_r_sorted = self._delta_r_sorted_vmap(
+            sorted_r, self.numbers, weights, means, stds
+        )
 
         delta_r_original = torch.empty_like(delta_r_sorted)
 
         delta_r_original.scatter_(dim=0, index=idx, src=delta_r_sorted)
 
         delta_x = (u[None, :] * delta_r_original[:, None]) / self.number_unit_vectors
-        return delta_x        
+        return delta_x
 
     @torch.compile
     def sample(self, weights: Tensor, means: Tensor, covariances: Tensor):
-        
+        """
+        Sample from the Gaussian Mixture. Returns a (L, N) tensor of samples.
+
+        :param weights: (T,)
+        :type weights: Tensor
+        :param means: (T, N)
+        :type means: Tensor
+        :param covariances: (T, N, N)
+        :type covariances: Tensor
+        """
+
         # 1. Create projections of the original GM onto the unit vectors.
         projected_means = self.unit_vectors @ means.T  # -> (K, L)
 
-        sigma2 = torch.einsum("kd,lde,ke->kl", self.unit_vectors, covariances, self.unit_vectors)
+        sigma2 = torch.einsum(
+            "kd,lde,ke->kl", self.unit_vectors, covariances, self.unit_vectors
+        )
         sigma2 = torch.clamp(sigma2, min=1e-3)
         projected_stds = torch.sqrt(sigma2)
 
@@ -146,7 +192,10 @@ class PCDSampler:
 
         # 3. Start the minimization loop
         for _ in range(self.steps):
-            coef = self.alpha_min * _ / self.steps + self.alpha_max * (self.steps - _) / self.steps
+            coef = (
+                self.alpha_min * _ / self.steps
+                + self.alpha_max * (self.steps - _) / self.steps
+            )
 
             # Calculate projections onto unit vectors before hand
             R = self.unit_vectors @ X.T  # -> (K, L)
@@ -158,7 +207,9 @@ class PCDSampler:
                 dim=0
             )  # -> (L, N)
 
-            if torch.norm(delta_x.sum()) < self.threshold:  # works better with sum() and then summing over a vector.
+            if (
+                torch.norm(delta_x.sum()) < self.threshold
+            ):  # works better with sum() and then summing over a vector.
                 return X
             X += coef * delta_x
 
